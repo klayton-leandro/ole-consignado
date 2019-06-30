484cd36e69c7db3374283fd4944c76de016c216e
klayton-leandro <emprestimo-fixo@bol.com.br>
2019-06-30 15:18:45 -0300

    painel score

--- Desktop/score-site/config/app.php
+++ Desktop/score-site/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Score Painel'),
+    'name' => env('APP_NAME', 'PAINEL SCORE'),
 
     /*
     |--------------------------------------------------------------------------
